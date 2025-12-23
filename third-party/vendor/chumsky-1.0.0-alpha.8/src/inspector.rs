82402e6a26e406cf791b0202704132b22dc00d43
Steve Klabnik <steve@steveklabnik.com>
2025-12-22 19:43:12 -0600

    Vendor third-party dependencies for reproducible builds
    Commit the vendored sources, Cargo.lock, and .cargo/config.toml to
    ensure builds work without network access and without needing to run
    reindeer. This follows reindeer best practices for monorepos.
    
    Previously, the vendor directory was gitignored, causing fresh clones
    to fail with mismatched dependency versions (fixes #287).
    
    Changes:
    - Remove vendor/, Cargo.lock, .cargo/ from .gitignore
    - Keep .cargo/registry/ ignored (redundant with vendored sources)
    - Increase jj snapshot.max-new-file-size for Windows import libraries
    - Add ~90 vendored crates

---
+++ third-party/vendor/chumsky-1.0.0-alpha.8/src/inspector.rs
@@ -0,0 +1,79 @@
+//! Parser extensions that inspect the input without modifying it.
+//!
+//! *"Only one man stood and watched the sky, stood with terrible sadness in his eyes
+//! and rubber bungs in his ears. He knew exactly what was happening and had known
+//! ever since his Sub-Etha Sens-O-Matic had started winking in the dead of night
+//! beside his pillar and woken him with a start."*
+use crate::{
+    input::{Checkpoint, Cursor},
+    Input,
+};
+use core::ops::{Deref, DerefMut};
+
+#[allow(unused)] // for intra-doc links
+use crate::Parser;
+
+/// A type that receives event hooks when certain parsing actions occur.
+///
+/// If you don't need to receive event hooks, use [`SimpleState`].
+pub trait Inspector<'src, I: Input<'src>> {
+    /// A type the Inspector can use to revert to a previous state.
+    ///
+    /// For implementation reasons, this is required to be `Copy + Clone`.
+    type Checkpoint: Copy + Clone;
+
+    /// This function is called when a new token is read from the input stream.
+    // impl note: this should be called only when `self.cursor` is updated, not when we only peek at the next token.
+    fn on_token(&mut self, token: &I::Token);
+    /// This function is called when a combinator saves the current state of the parse.
+    fn on_save<'parse>(&self, cursor: &Cursor<'src, 'parse, I>) -> Self::Checkpoint;
+    /// This function is called when a combinator rewinds to an earlier state of the parser.
+    ///
+    /// You can use [`Checkpoint::inspector`] to get back the [`Checkpoint`][Self::Checkpoint]
+    /// you originally created in [`on_save`][Self::on_save].
+    fn on_rewind<'parse>(&mut self, marker: &Checkpoint<'src, 'parse, I, Self::Checkpoint>);
+}
+
+impl<'src, I: Input<'src>> Inspector<'src, I> for () {
+    type Checkpoint = ();
+    #[inline(always)]
+    fn on_token(&mut self, _: &<I as Input<'src>>::Token) {}
+    #[inline(always)]
+    fn on_save<'parse>(&self, _: &Cursor<'src, 'parse, I>) -> Self::Checkpoint {}
+    #[inline(always)]
+    fn on_rewind<'parse>(&mut self, _: &Checkpoint<'src, 'parse, I, Self>) {}
+}
+
+/// A state type that should be accessible directly from `parser.state()` and has no special behavior.
+///
+/// This wrapper implements the [`Inspector`] trait for you so you don't have to.
+pub struct SimpleState<T>(pub T);
+impl<'src, T, I: Input<'src>> Inspector<'src, I> for SimpleState<T> {
+    type Checkpoint = ();
+    #[inline(always)]
+    fn on_token(&mut self, _: &<I as Input<'src>>::Token) {}
+    #[inline(always)]
+    fn on_save<'parse>(&self, _: &Cursor<'src, 'parse, I>) -> Self::Checkpoint {}
+    #[inline(always)]
+    fn on_rewind<'parse>(&mut self, _: &Checkpoint<'src, 'parse, I, Self::Checkpoint>) {}
+}
+
+impl<T> Deref for SimpleState<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for SimpleState<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T> From<T> for SimpleState<T> {
+    fn from(value: T) -> Self {
+        Self(value)
+    }
+}
---
+++ third-party/vendor/chumsky-1.0.0-alpha.8/src/stream.rs
@@ -0,0 +1,202 @@
+use super::*;
+
+/// An input that dynamically pulls tokens from an [`Iterator`].
+///
+/// Internally, the stream will pull tokens in batches so as to avoid invoking the iterator every time a new token is
+/// required.
+pub struct Stream<I: Iterator> {
+    tokens: Vec<I::Item>,
+    iter: I,
+}
+
+impl<I: Iterator> Stream<I> {
+    /// Create a new stream from an [`Iterator`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use chumsky::{prelude::*, input::Stream};
+    /// let stream = Stream::from_iter((0..10).map(|i| char::from_digit(i, 10).unwrap()));
+    ///
+    /// let parser = any::<_, extra::Err<Simple<_>>>().filter(|c: &char| c.is_ascii_digit()).repeated().collect::<String>();
+    ///
+    /// assert_eq!(parser.parse(stream).into_result().as_deref(), Ok("0123456789"));
+    /// ```
+    pub fn from_iter<J: IntoIterator<IntoIter = I>>(iter: J) -> Self {
+        Self {
+            tokens: Vec::new(),
+            iter: iter.into_iter(),
+        }
+    }
+
+    /// Box this stream, turning it into a [BoxedStream]. This can be useful in cases where your parser accepts input
+    /// from several different sources and it needs to work with all of them.
+    pub fn boxed<'a>(self) -> BoxedStream<'a, I::Item>
+    where
+        I: 'a,
+    {
+        Stream {
+            tokens: self.tokens,
+            iter: Box::new(self.iter),
+        }
+    }
+
+    /// Like [`Stream::boxed`], but yields an [`BoxedExactSizeStream`], which implements [`ExactSizeInput`].
+    pub fn exact_size_boxed<'a>(self) -> BoxedExactSizeStream<'a, I::Item>
+    where
+        I: ExactSizeIterator + 'a,
+    {
+        Stream {
+            tokens: self.tokens,
+            iter: Box::new(self.iter),
+        }
+    }
+}
+
+/// A stream containing a boxed iterator. See [`Stream::boxed`].
+pub type BoxedStream<'a, T> = Stream<Box<dyn Iterator<Item = T> + 'a>>;
+
+/// A stream containing a boxed exact-sized iterator. See [`Stream::exact_size_boxed`].
+pub type BoxedExactSizeStream<'a, T> = Stream<Box<dyn ExactSizeIterator<Item = T> + 'a>>;
+
+impl<I: Iterator> Sealed for Stream<I> {}
+impl<'a, I: Iterator + 'a> Input<'a> for Stream<I>
+where
+    I::Item: Clone,
+{
+    type Span = SimpleSpan<usize>;
+
+    type Token = I::Item;
+    type MaybeToken = I::Item;
+
+    type Cursor = usize;
+
+    type Cache = Self;
+
+    #[inline(always)]
+    fn begin(self) -> (Self::Cursor, Self::Cache) {
+        (0, self)
+    }
+
+    #[inline]
+    fn cursor_location(cursor: &Self::Cursor) -> usize {
+        *cursor
+    }
+
+    #[inline(always)]
+    unsafe fn next_maybe(
+        this: &mut Self::Cache,
+        cursor: &mut Self::Cursor,
+    ) -> Option<Self::MaybeToken> {
+        Self::next(this, cursor)
+    }
+
+    #[inline(always)]
+    unsafe fn span(_this: &mut Self::Cache, range: Range<&Self::Cursor>) -> Self::Span {
+        (*range.start..*range.end).into()
+    }
+}
+
+impl<'a, I: ExactSizeIterator + 'a> ExactSizeInput<'a> for Stream<I>
+where
+    I::Item: Clone,
+{
+    #[inline(always)]
+    unsafe fn span_from(this: &mut Self::Cache, range: RangeFrom<&Self::Cursor>) -> Self::Span {
+        (*range.start..this.tokens.len() + this.iter.len()).into()
+    }
+}
+
+impl<'a, I: Iterator + 'a> ValueInput<'a> for Stream<I>
+where
+    I::Item: Clone,
+{
+    #[inline]
+    unsafe fn next(this: &mut Self::Cache, cursor: &mut Self::Cursor) -> Option<Self::Token> {
+        // Pull new items into the vector if we need them
+        if this.tokens.len() <= *cursor {
+            this.tokens.extend((&mut this.iter).take(512));
+        }
+
+        // Get the token at the given cursor
+        this.tokens.get(*cursor).map(|tok| {
+            *cursor += 1;
+            tok.clone()
+        })
+    }
+}
+
+/// An input type that uses an iterator to generate tokens.
+///
+/// This input type supports backtracking by duplicating the iterator. It is recommended that your iterator is very
+/// cheap to copy/clone.
+pub struct IterInput<I, S> {
+    iter: I,
+    eoi: S,
+}
+
+impl<I, S> IterInput<I, S> {
+    /// Create a new [`IterInput`] with the given iterator, and end of input span.
+    pub fn new(iter: I, eoi: S) -> Self {
+        Self { iter, eoi }
+    }
+}
+
+impl<'src, I, T: 'src, S> Input<'src> for IterInput<I, S>
+where
+    I: Iterator<Item = (T, S)> + Clone + 'src,
+    S: Span + 'src,
+{
+    type Cursor = (I, usize, Option<S::Offset>);
+    type Span = S;
+
+    type Token = T;
+    type MaybeToken = T;
+
+    type Cache = S; // eoi
+
+    #[inline]
+    fn begin(self) -> (Self::Cursor, Self::Cache) {
+        ((self.iter, 0, None), self.eoi)
+    }
+
+    #[inline]
+    fn cursor_location(cursor: &Self::Cursor) -> usize {
+        cursor.1
+    }
+
+    unsafe fn next_maybe(
+        _eoi: &mut Self::Cache,
+        cursor: &mut Self::Cursor,
+    ) -> Option<Self::MaybeToken> {
+        cursor.0.next().map(|(tok, span)| {
+            cursor.1 += 1;
+            cursor.2 = Some(span.end());
+            tok
+        })
+    }
+
+    unsafe fn span(eoi: &mut Self::Cache, range: Range<&Self::Cursor>) -> Self::Span {
+        let start = range
+            .start
+            .0
+            .clone()
+            .next()
+            .map(|(_, s)| s.start())
+            .unwrap_or_else(|| eoi.start());
+        let end = range.end.2.clone().unwrap_or_else(|| eoi.end());
+        S::new(eoi.context(), start..end)
+    }
+}
+
+#[test]
+fn map_tuple() {
+    fn parser<'src, I: Input<'src, Token = char>>() -> impl Parser<'src, I, char> {
+        just('h')
+    }
+
+    let stream = Stream::from_iter(core::iter::once(('h', 0..1))).boxed();
+    let stream = stream.map(0..10, |(t, s)| (t, s));
+
+    assert_eq!(parser().parse(stream).into_result(), Ok('h'));
+}
